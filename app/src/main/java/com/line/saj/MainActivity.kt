11f7f8172ea5f0e796525367887be4258e0f0bd1
손아진 <sonajin11@gmail.com>
2020-02-10 16:02:50 +0900

    Init Project
    init project

---
+++ app/src/main/java/com/line/saj/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.line.saj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
